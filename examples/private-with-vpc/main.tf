3c4b7c3d90d0cce7725b826a772d613695726f52
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2025-01-15 20:29:45 +0900

    fix : change module example name

--- examples/private-with-vpc/main.tf
+++ examples/private-with-vpc/main.tf
@@ -4,7 +4,7 @@
 ##   name = "test-private-vpc"
 ## }
 
-module "database_instance" {
+module "test_instance" {
   source = "../../database-instance"
 
   name              = "test-sql-instance" # Cloud SQL 인스턴스 이름
@@ -22,21 +22,21 @@ module "database_instance" {
 }
 
 # Cloud SQL 인스턴스에 데이터베이스 정의
-module "database" {
+module "test_database" {
   source = "../../database"
 
   name            = "test-database"               # 데이터베이스 이름
-  instance        = module.database_instance.name # 앞서 생성한 인스턴스 이름 참조
+  instance        = module.test_instance.name # 앞서 생성한 인스턴스 이름 참조
   charset         = "UTF8"                        # 문자 집합
   collation       = "en_US.UTF8"                  # 정렬 설정
   deletion_policy = "DELETE"                      # 삭제 정책 (DELETE 또는 ABANDON)
 }
 
 # Cloud SQL 데이터베이스 사용자 정의
-module "database_user" {
+module "test_database_user" {
   source = "../../database-user"
 
-  instance = module.database_instance.name # 앞서 생성한 인스턴스 이름 참조
+  instance = module.test_instance.name # 앞서 생성한 인스턴스 이름 참조
   name     = "test-user"                   # 사용자 이름
   password = "test-password"               # 사용자 비밀번호
   type     = "BUILT_IN"                    # 인증 유형 (BUILT_IN, CLOUD_IAM_USER 등)
--- examples/private-with-vpc/outputs.tf
+++ examples/private-with-vpc/outputs.tf
@@ -1,23 +1,23 @@
 output "database_instance_details" {
   description = "Cloud SQL 인스턴스 세부 정보"
   value = {
-    id                   = module.database_instance.id                   # Cloud SQL 인스턴스의 고유 ID
-    name                 = module.database_instance.name                 # Cloud SQL 인스턴스의 이름
-    self_link            = module.database_instance.self_link            # Cloud SQL 인스턴스의 URI
-    connection_name      = module.database_instance.connection_name      # Cloud SQL 인스턴스의 연결 이름
-    region               = module.database_instance.region               # Cloud SQL 인스턴스가 위치한 리전
-    project              = module.database_instance.project              # Cloud SQL 인스턴스가 속한 프로젝트 ID
-    database_version     = module.database_instance.database_version     # 데이터베이스 버전
-    tier                 = module.database_instance.tier                 # 머신 타입
-    disk_size            = module.database_instance.disk_size            # 디스크 크기 (GB)
-    availability_type    = module.database_instance.availability_type    # 고가용성 유형
-    ip_addresses         = module.database_instance.ip_addresses         # IP 주소 목록
-    private_network      = module.database_instance.private_network      # 프라이빗 네트워크
-    ipv4_enabled         = module.database_instance.ipv4_enabled         # IPv4 활성화 여부
-    backup_configuration = module.database_instance.backup_configuration # 백업 설정
-    query_insights       = module.database_instance.query_insights       # Query Insights 설정
-    maintenance_window   = module.database_instance.maintenance_window   # 유지 관리 창
-    labels               = module.database_instance.labels               # 사용자 정의 레이블       # 복제본 이름 목록
-    deletion_protection  = module.database_instance.deletion_protection  # 삭제 보호 상태
+    id                   = module.test_instance.id                   # Cloud SQL 인스턴스의 고유 ID
+    name                 = module.test_instance.name                 # Cloud SQL 인스턴스의 이름
+    self_link            = module.test_instance.self_link            # Cloud SQL 인스턴스의 URI
+    connection_name      = module.test_instance.connection_name      # Cloud SQL 인스턴스의 연결 이름
+    region               = module.test_instance.region               # Cloud SQL 인스턴스가 위치한 리전
+    project              = module.test_instance.project              # Cloud SQL 인스턴스가 속한 프로젝트 ID
+    database_version     = module.test_instance.database_version     # 데이터베이스 버전
+    tier                 = module.test_instance.tier                 # 머신 타입
+    disk_size            = module.test_instance.disk_size            # 디스크 크기 (GB)
+    availability_type    = module.test_instance.availability_type    # 고가용성 유형
+    ip_addresses         = module.test_instance.ip_addresses         # IP 주소 목록
+    private_network      = module.test_instance.private_network      # 프라이빗 네트워크
+    ipv4_enabled         = module.test_instance.ipv4_enabled         # IPv4 활성화 여부
+    backup_configuration = module.test_instance.backup_configuration # 백업 설정
+    query_insights       = module.test_instance.query_insights       # Query Insights 설정
+    maintenance_window   = module.test_instance.maintenance_window   # 유지 관리 창
+    labels               = module.test_instance.labels               # 사용자 정의 레이블       # 복제본 이름 목록
+    deletion_protection  = module.test_instance.deletion_protection  # 삭제 보호 상태
   }
 }
